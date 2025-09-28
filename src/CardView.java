916d9afd765b1b1035d35ed8b1c3195d7396fd2e
Lucas <lkelly894@gmail.com>
2025-09-28 16:04:01 +0100

    - added drop shadow to cards; - cards now have round corners; - rank/suit is displayed vertically;

--- src/CardView.java
+++ src/CardView.java
@@ -4,6 +4,7 @@ import javafx.beans.property.SimpleBooleanProperty;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.Node;
+import javafx.scene.effect.DropShadow;
 import javafx.scene.layout.StackPane;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Rectangle;
@@ -35,6 +36,13 @@ public class CardView extends StackPane {
 
   private Node createCardBackground() {
     var cardBackground = new Rectangle(CARD_WIDTH, CARD_HEIGHT);
+    cardBackground.setArcWidth(12.5d);
+    cardBackground.setArcHeight(12.5d);
+
+    //test shadow
+    DropShadow ds = new DropShadow(2, -.7, 2, Color.DARKGRAY);
+    cardBackground.setEffect(ds);
+
     cardBackground.fillProperty().bind(
         Bindings.createObjectBinding(() -> isFaceUp.get() ? Color.GHOSTWHITE : Color.TOMATO, isFaceUp)
     );
@@ -42,10 +50,11 @@ public class CardView extends StackPane {
   }
 
   private Node createCardLabel(String cardText, Color color, Pos position, double rotation) {
+    //TODO: sort alignment of rank/suit? margins?
     var label = new Text(cardText);
     label.setFill(color);
     StackPane.setAlignment(label, position);
-    StackPane.setMargin(label, new Insets(10));
+    StackPane.setMargin(label, new Insets(5));
     label.setRotate(rotation);
     return label;
   }
